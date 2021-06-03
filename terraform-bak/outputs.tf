9d419debed7d528fe6d664a55b97dd3d0abd660c
Antonio Goncalves <antonio.goncalves@gmail.com>
2021-06-03 17:05:09 +0200

    clean terraform

--- terraform-bak/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "application_hostname" {
-  value       = module.application.application_hostname
-  description = "The Web application URL."
-}
-
-output "resource_group" {
-  value       = local.resource_group
-  description = "The resource group."
-}
